ed3c5598f845bf4487f082ca9c191a23905cb0b1
Dmitriy Makarov <DMVmakarov2000@gmail.com>
2020-06-15 03:09:03 +0500

    first commit

---
+++ Practice-7Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Practice-7Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Practice-7Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0d469c6-916a-4a02-986a-1b7e95f5dd04")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Practice-7Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practice_7;
+
+namespace Practice_7Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // arrange 
+            int n = 3;
+            int[,] GrayCode = new int[Convert.ToInt32(Math.Pow(2, n)), n];
+            // act
+            GrayCode = Program.BuildGrayCode(3, GrayCode);
+            // assert
+            Assert.AreEqual(GrayCode[0, 0] == 0, true);
+            Assert.AreEqual(GrayCode.GetLength(1), n);
+            Assert.AreEqual(GrayCode.GetLength(0), Convert.ToInt32(Math.Pow(2, n)));
+        }
+    }
+}
